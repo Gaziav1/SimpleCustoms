24afb1e6043c0eeb4cb5085677bc720953b84bb8
Gaziav <gaziavishakov2211@icloud.com>
2019-09-13 21:25:57 +0300

    Animations improvments

---
+++ SimpleCustoms/CustomsRulesScreen/View/CustomsRulesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomsRulesTableViewCell.swift
+//  SimpleCustoms
+//
+//  Created by Газияв Исхаков on 12/09/2019.
+//  Copyright © 2019 Газияв Исхаков. All rights reserved.
+//
+
+import UIKit
+
+class CustomsRulesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
